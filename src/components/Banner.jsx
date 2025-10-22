087b4b993ff6ade232942b960ce2a6526b609959
synan-mannan <syedmark786@gmail.com>
2025-10-22 15:58:03 +0530

    Added Carousel for Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import axios from 'axios'
 const Banner = () => {
+  const [movienum, setmovienum] = useState(2);
   const [moviebanner, setmoviebanner] = useState('https://th.bing.com/th/id/OIP.L2OAkdDZ99c9uVVE6whGxgHaEo?w=259&h=180&c=7&r=0&o=7&cb=12&dpr=1.3&pid=1.7&rm=3')
   const [movietitle, setmovietitle] = useState('placeholder movie')
   useEffect(() => {
-    axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=3d93e932ab849a6f3f022b944dc8f8a1').then(response => {
-      setmoviebanner(`https://image.tmdb.org/t/p/original${response.data.results[1].poster_path}`);
-      setmovietitle(response.data.results[1].title)
-    }).catch(e => {
-      console.error(e);
+  axios
+    .get('https://api.themoviedb.org/3/trending/movie/day?api_key=3d93e932ab849a6f3f022b944dc8f8a1')
+    .then(response => {
+      setInterval(() => {
+        setmovienum(prevNum => {
+          const newNum = (prevNum + 1) % response.data.results.length;
+          const movie = response.data.results[newNum];
+          setmoviebanner(`https://image.tmdb.org/t/p/original${movie.poster_path}`);
+          setmovietitle(movie.title);
+          return newNum;
+        });
+      }, 5000);
     })
-  }, [])
+    .catch(console.error);
+}, []);
   return (
     <div
         className='h-[20vh] sm:h-[75vh] bg-contain bg-no-repeat bg-center flex items-end'
